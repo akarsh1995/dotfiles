c1aed78826d9842a469f448eb56a0609f55c9bdb
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-05-04 09:00:00 +0530

    2025-05-04

--- bootstrap.fish
+++ bootstrap.fish
@@ -2,4 +2,10 @@
 begin
     crontab -l 2>/dev/null
     echo "0 * * * * $(which fish) -c 'cd $HOME/.config && if test (git status --short | wc -l) -gt 0; git add -A; git commit -m \"\$(date +'\%Y-\%m-\%d')\"; git push origin main; end'"
+    echo "0 0 * * * $(which docker) container prune -f"
 end | crontab -
+
+# This script sets up a git configuration file with user information.
+echo "[user]
+name = Akarsh Jain
+email = akarsh.1995.02@gmail.com" >$HOME/.gitconfig

0e15ab03a011e7ccb1e9656cf26c2197e1af9529
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-05-07 18:00:00 +0530

    2025-05-07

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
+    fish_add_path ~/.local/share/nvm/*/bin
     generate_docker_aliases $AYR_DIR/integration/docker-compose.yml
-    alias eu-bhishma "docker-compose -f $AYR_DIR/integration/docker-compose.yml up"
-    alias ed "docker-compose -f $AYR_DIR/integration/docker-compose.yml down"
+    alias eu-bhishma "docker-compose -f $AYR_DIR/integration/docker-compose.yml --profile=be-lite up -d"
+    alias eu-fe "docker-compose -f $AYR_DIR/integration/docker-compose.yml --profile=fe up -d"
+    alias ed "docker-compose -f $AYR_DIR/integration/docker-compose.yml down --remove-orphans"
     fzf_configure_bindings --directory=\cf --variables=\e\cv
-    nvm use v20.13.1
 end
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     formatters_by_ft = {
       sql = { "pg_format" },
+      typescript = {},
     },
   },
 }

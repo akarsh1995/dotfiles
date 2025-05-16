544bb1be610bcefd5eb12a94c4aa046e8b19f0bc
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-05-16 18:00:00 +0530

    2025-05-16

--- fish/conf.d/02-aliases.fish
+++ fish/conf.d/02-aliases.fish
@@ -1,2 +1,3 @@
 alias ls 'nnn -de'
 alias ghcs 'gh copilot suggest -t shell'
+alias open_recent_pr 'gh pr list --state open --author "@me" --json number | jq -r ".[0].number" | xargs gh pr view --web'

ba993f367ff8006eaaf60c7104c3c06dde39f780
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-05-22 22:54:33 +0530

    Consolidate file encryption related functions and completions

--- fish/completions/file_encrypt.fish
+++
@@ -1,4 +0,0 @@
-# Completions for file_encrypt function
-complete -c file_encrypt -f -d "Encrypt a file and add to registry"
-complete -c file_encrypt -a "(find . -type f -not -path '*/\.git/*')"
-complete -c file_encrypt -l remove -d "Remove the original file after encryption without prompting"
--- fish/completions/file_list_encrypted.fish
+++
@@ -1,3 +0,0 @@
-# Completions for file_list_encrypted function
-complete -c file_list_encrypted -f -d "List all encrypted files in registry"
-complete -c file_list_encrypted -l details -d "Show additional details including modification times"

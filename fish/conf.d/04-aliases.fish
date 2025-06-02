8e19d90ef1568c84c609e72745dedcc7589b55cc
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-06-02 18:00:02 +0530

    Auto commit on 2025-06-02 18:00:02 on branch main

--- fish/conf.d/04-aliases.fish
+++ fish/conf.d/04-aliases.fish
@@ -2,6 +2,7 @@ alias ghcs 'gh copilot suggest -t shell'
 alias open_recent_pr 'gh pr list --state open --author "@me" --json number | jq -r ".[0].number" | xargs gh pr view --web'
 alias gp 'git push origin HEAD'
 alias open_repo 'gh repo view --web'
+alias ls 'eza --icons --group-directories-first --git --color=always'
 
 # user defined aliases that are dependent on environment variables
 function credit_test_user

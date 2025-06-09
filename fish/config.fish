ea3820076e4e5557175fcf4b25ee57ca3aae2336
Akarsh Jain <akarsh.1995.02@gmail.com>
2025-06-09 18:00:01 +0530

    Auto commit on 2025-06-09 18:00:01 on branch main

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # set -g pwstore_gpg_recipient Akarsh Jain
     generate_docker_aliases $AYR_DIR/integration/docker-compose.yml be-lite
     fzf_configure_bindings --directory=\cf --variables=\e\cv
+    fish_add_path -g $HOME/.local/bin
 end

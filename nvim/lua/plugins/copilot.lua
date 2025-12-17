6ab0aec12a36fbce1959d76751c86a8430ef04fd
Akarsh Jain <25827628+akarsh1995@users.noreply.github.com>
2025-12-17 18:00:01 +0530

    Auto commit on 2025-12-17 18:00:01 on branch main

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/fnm/node-versions/v25.2.1/installation/bin/node",
     server_opts_overrides = {
       settings = {
         telemetry = {

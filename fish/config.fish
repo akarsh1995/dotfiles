bd58ca3e064d67d10e7820dbdc8f470522a89cfb
Akarsh Jain <25827628+akarsh1995@users.noreply.github.com>
2026-02-17 18:00:01 +0530

    Auto commit on 2026-02-17 18:00:01 on branch main

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     generate_docker_aliases $AYR_DIR/integration/docker-compose.yml be-lite
     fzf_configure_bindings --directory=\cf --variables=\e\cv
     fish_add_path -g $HOME/.local/bin
-    set -x PSQL_CONNECTION (echo $DBEE_CONNECTIONS | jq -r ".[0].url") 
+    set -x PSQL_CONNECTION (echo $DBEE_CONNECTIONS | jq -r ".[0].url")
+    alias sync_private_fork "git fetch upstream && git rebase upstream/main"
 end
